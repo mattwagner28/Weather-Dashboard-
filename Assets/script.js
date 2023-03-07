e18843742f0829f9d6d6f56669e3d646b857a4f1
MattWagner28 <matthewwagner@outlook.com>
2023-03-06 19:57:40 -0500

    added temp, and wind speed for 5-day forecast

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,21 @@
-const userInput = document.getElementById("user-input");
-const searchButton = document.querySelector("#search");
-const currentWeather = document.querySelector("#current-weather");
+var userInput = document.getElementById("user-input");
+var searchButton = document.querySelector("#search");
+var currentWeather = document.querySelector("#current-weather");
 
 function getApi(event) {
     event.preventDefault();
-    const searchTerms = userInput.value.replaceAll(" ", "");
-    const apiKey = '527555cb025cd56f732a616f11737fe5';
-    const apiCity = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchTerms + '&limit=' + 1 + '&appid=' + apiKey;
+    var searchTerms = userInput.value.replaceAll(" ", "");
+    var apiKey = '527555cb025cd56f732a616f11737fe5';
+    var apiCity = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchTerms + '&limit=' + 1 + '&appid=' + apiKey;
     
         fetch(apiCity)
         .then(function (data) {
             return data.json();
         })
         .then(function (data) {
-            const latitude = data[0].lat;
-            const longitude = data[0].lon;
-            const weatherApi = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
+            var latitude = data[0].lat;
+            var longitude = data[0].lon;
+            var weatherApi = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey + '&units=imperial';
 
             fetch(weatherApi)
             .then(function (forecast) {
@@ -24,25 +24,37 @@ function getApi(event) {
             .then(function (forecast) {
             console.log(forecast);
 
-                const cityName = forecast.city.name;
-                const cityHeading = document.createElement("h2");
+                var cityName = forecast.city.name;
+                var cityHeading = document.createElement("h2");
                 cityHeading.textContent = cityName;
                 currentWeather.appendChild(cityHeading);
 
-        
-                for (i = 0; i < 41; i++) {
-                    if (i % 8 === 0) {
-                date = dayjs(forecast.list[i].dt_txt).format("dddd MMMM D, YYYY");
-                console.log(date);
-                const dateText = document.createElement("p");
-                dateText.textContent = date;
-                currentWeather.appendChild(dateText);
+                
+                for (i = 0; i < 41; i+=8) {
 
-                // console.log(dayjs(date).format("D, M, YYYY"));
-                }}
+                        //adds date
+                        var date = dayjs(forecast.list[i].dt_txt).format("dddd MMMM D, YYYY");   
+                        var dateText = document.createElement("p");
+                        dateText.textContent = date;
+                        document.getElementById("day-div" + i).appendChild(dateText);
+
+                        // adds main weather description
+
+                        //adds degrees in Farenheit 
+                        var temp = forecast.list[i].main.temp;
+                        var tempText = document.createElement("p");
+                        tempText.textContent = "Temp: " + temp + " °F";
+                        document.getElementById("day-div" + i).appendChild(tempText);
+
+                        //adds Wind speed
+                        var wind = forecast.list[i].wind.speed;
+                        var windText = document.createElement("p");
+                        windText.textContent = "Wind: " + Math.round((Number(wind) * 2.23694)) + " MPH";
+                        document.getElementById("day-div" + i).appendChild(windText);
+
+                }
+
             })
-
-
 
     })
 }
